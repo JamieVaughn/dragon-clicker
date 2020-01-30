ecb4d6192795d415b8907c227700b3df8e819999
JamieVaughn <wxv6@case.edu>
2020-01-30 17:30:42 -0500

    Add stateful solution

--- index.js
+++ index.js
@@ -1,7 +1,46 @@
-const main = document.querySelector('main')
+let main = document.querySelector('main')
+let list = document.querySelector('ul')
+const getDragon = (name) => name ==='Fluffykins' ? '🐇':'🐉'
 
-let myDragon;
 
-myDragon.addEventListener('click', function(e){
-    //handle clicks
+let dragons = [{id: 0, type: 'Forest', clicks: 0}]
+
+
+let dragonCard = dragon => `
+<div class=${dragon.type} data-id=${dragon.id}>
+  <h1>${dragon.type} Dragon</h1>
+  <div onclick='increment(${dragon.id})'>
+${getDragon(dragon.type)}
+<span>${dragon.clicks}</span></div>
+</div>`
+
+
+list.addEventListener('click', (e) => {
+  if(e.target.tagName.match(/li/i)){
+    let dragonType = e.target.innerText.split(' ')[0];
+    dragons.push({
+      id: dragons.length,
+      type: dragonType,
+      clicks: 0
+    })
+    console.log(dragons)
+        render()
+  }
 })
+
+
+function increment(id){
+    let cur = dragons.filter(d => d.id == id)[0]
+    cur.clicks += 1
+    render()
+}
+
+function render() {
+  main.innerHTML = ''
+  dragons.forEach(d => {
+    console.log(d)
+    main.innerHTML += dragonCard(d)
+  })
+}
+
+render()
